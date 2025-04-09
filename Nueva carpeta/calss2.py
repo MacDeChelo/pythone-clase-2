2802382a28c4d9329e6b813dbf6fe483d8147669
MacDeChelo <marcelogutierrez651@gmail.com>
2025-04-09 14:47:34 -0400

    ejercicio python

---
+++ Nueva carpeta/calss2.py
@@ -0,0 +1,8 @@
+# pedir al usuario numeros, para ingresar en una lista#
+n= int(input(" ingrese numerow para ingresar a la lista\n"))
+n2= int(input(" ingrese numerow para ingresar a la lista\n"))
+n3= int(input(" ingrese numerow para ingresar a la lista\n"))
+listN= [n,n2,n3]
+print(listN)
+
+
